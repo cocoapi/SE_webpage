88ca65d9f2e64b7bd08d66acef0d42f112020457
soonHy <soondl@gmail.com>
2018-03-23 21:08:57 +0900

    이미지 출력 연습#1

--- src/components/first.js
+++ src/components/first.js
@@ -4,7 +4,7 @@ class First extends Component {
   render() {
     return (
       <div>
-		<p> Hello, React! </p>
+		<img src="https://s3.amazonaws.com/s3.imagefinder.co/uploads/2015/12/21000845/1300x867x00062-1300x867.jpg.pagespeed.ic_.nhyDu09kSW.jpg" alt="codingImage" width="100%"/>
 	  </div>
     );
   }
