12570c8a358a640c5b0659a5ebf70cca1af6bed6
cocoapi <eksrns22tp@gmail.com>
2018-03-23 21:07:42 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import First from './components/first'
+import Second from './components/second'
+
 
 class App extends Component {
   render() {
     return (
-	<div classname='App'>
-		<p> hello, react! </p>
-	</div>
+		<div classname="App">
+			<p> Hello, React! </p>
+			<div style={{
+				height: '20%',
+			}}>
+				<First />
+			</div>
+			<div style={{
+				height: '20%',
+			}}>
+			<Second />
+			</div>
+		</div>
 	);
   }
 }
