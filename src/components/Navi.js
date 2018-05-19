befd56f80cc9f6a178e366045fad57edaafc58ec
cocoapi <eksrns22tp@gmail.com>
2018-05-19 15:41:23 +0900

    update Navi bar
    네이게이션바에 scroll 이벤트 리스너를통해 상단의 버튼은 스크롤이
    움직이더라도 고정되도록 설정 #23
    마크를 클릭할시 프론트페이지로 라우팅되도록 변경

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -2,16 +2,36 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Navi extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			sticky: false,
+		}
+	}
+	componentDidMount() {
+		window.addEventListener("scroll", this.onScroll);	
+	}
+	componentWillUnmount() {
+		window.removeEventListener("scroll", this.onScroll);
+	}
+	onScroll = (e) => {
+		if (window.scrollY >= 200 && !this.state.sticky) {
+			this.setState({sticky: true});
+		} else if (window.scrollY < 200 && this.state.sticky) {
+			this.setState({sticky: false});
+		}
+	}
   render() {
     return (
     <div style={{width:'100%', height:'250px',  display:'flex', flexDirection:'column'}}>
-			<div style={{height:'80%', display:'flex', flexDirection:'row'}}>
+			<div style={{height:'200px', display:'flex', flexDirection:'row'}}>
 				<div style={{width:'33%'}}/>
 				<div style={{width:'33%', overflow:'hidden'}}>
-					<img width='100%' src='https://www.kw.ac.kr/_res/ko/img/intro/symbol01_06.jpg' alt='Mark'/>
+					<Link to="/"><img width='100%' src='https://www.kw.ac.kr/_res/ko/img/intro/symbol01_06.jpg' alt='Mark'/></Link>
 				</div>
 			</div>
-			<div style={{height:'20%', display:'flex', flexDirection:'row'}}>
+			{this.state.sticky == false ? 
+			<div style={{height:'50px', display:'flex', flexDirection:'row', borderBottom:'1px solid black'}} >
 				<div style={{width:'33%', margin:'4px'}}>
 					<Link to={{ pathname: '/PlayStation', state: { consoleName: 'PS' }}}> PS4 </Link>
 				</div>
@@ -21,7 +41,21 @@ class Navi extends Component {
 				<div style={{width:'33%', margin:'4px'}}>
 					<Link to={{ pathname: '/XBOX', state: { consoleName: 'XBOX' }}}> XBOX </Link>
 				</div>
-			</div>
+				</div>
+				:
+				<div style={{width: '100%', height:'50px', display:'flex', flexDirection:'row', position:'fixed', top:'0px',
+				background:'white', borderBottom:'1px solid black', zIndex:'5'}}>
+					<div style={{width:'33%', margin:'4px'}}>
+						<Link to={{ pathname: '/PlayStation', state: { consoleName: 'PS' }}}> PS4 </Link>
+					</div>
+					<div style={{width:'33%', margin:'4px'}}>
+						<Link to={{ pathname: '/Nintendo', state: { consoleName: 'Nintendo' }}}> Nintendo </Link>
+					</div>
+					<div style={{width:'33%', margin:'4px'}}>
+						<Link to={{ pathname: '/XBOX', state: { consoleName: 'XBOX' }}}> XBOX </Link>
+					</div>
+				</div>
+			}
 	  </div>
     );
   }
