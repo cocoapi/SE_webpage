9741470196456a1c36197f6a80ccc8d1bdf25f93
soonHy <soondl@gmail.com>
2018-05-27 16:53:59 +0900

    Ads component update

--- src/components/Ads.js
+++ src/components/Ads.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Carousel, Row, Col } from 'antd';
+import '../App.css'
+
 
 class Ads extends Component {
   render() {
     return (
-      <div style={{ height: '300px' }}>
-        <p> Ads </p>
-      </div>
+      <Carousel autoplay>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+      </Carousel>
     );
   }
 }
