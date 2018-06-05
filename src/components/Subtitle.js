23eb0501b957b12c842c60a9770daca8e0dd6590
soonHy <soondl@gmail.com>
2018-06-06 05:21:50 +0900

    으으 머지...
    머지...

--- src/components/Subtitle.js
+++ src/components/Subtitle.js
@@ -2,22 +2,18 @@ import React, { Component } from 'react';
 import { Row, Col } from 'antd';
 
 const subTitle = {
-   // borderBottom: '1px solid gray',
     fontSize: '25px',
     paddingBottom: '30px',
     marginTop: '80px',
     fontWeight:'bold',
-    //textAlign: 'left'
   };
   
 class Subtitle extends Component {
     render() {
       return (
         <Row style={subTitle}>
-            {/* <Col span={4}> */}
                 {this.props.title}
                 {this.props.img}
-            {/* </Col> */}
         </Row>
       )
     }
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,16 +2,10 @@ import React, { Component } from 'react';
 import LoginForm from '../components/LoginForm';
 
 class Login extends Component {
-<<<<<<< HEAD
   componentDidMount() {
     window.scrollTo(0, 0)
   }
-=======
-  componentDidMount(){
-    window.scrollTo(0, 0)
-  }
-  
->>>>>>> bb8ddf162aefdcca00d4168f6a244eb8a2e29861
+
   render() {
     return (
       <div style={{ height: '800px', display: 'flex', flexDirection: 'column' }}>
